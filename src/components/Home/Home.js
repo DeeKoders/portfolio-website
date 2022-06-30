ddbc9d7a59e9c6a6a433e2f4f558155f55215db4
danyal-deekoders <deekoders@gmail.com>
2022-06-30 23:55:45 +0500

    Initial Commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import img from '../../images/danyal.png'
+import TypewriterComponent from 'typewriter-effect'
+const Home = () => {
+  return (
+    <div className='row' Style='min-height: 90vh'>
+      <div className='col-12 col-md-6 px-5 mt-3'>
+        <h1 className='row text-dark opacity-75 fs-4 mt-5 pt-5'>
+          Hello World !!!
+        </h1>
+        <div className='row'>
+          <h1 className='col-12 col-md-1 px-0 mx-0 fs-1'>I'm </h1>
+          <h1 className='col-12 col-md-10 px-0 mx-md-4 mx-0 fw-bold fs-1 text-danger'>
+            Danyal Ahmad
+          </h1>
+        </div>
+        <div className='row'>
+          <div className='col-12 col-md-4 px-0 mx-0 fw-bold fs-1 text-danger'>
+            <TypewriterComponent
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString('React JS')
+                  .pauseFor(2000)
+                  .deleteAll()
+                  .typeString('Flutter')
+                  .pauseFor(2000)
+                  .deleteAll()
+                  .typeString('React JS')
+                  .pauseFor(2000)
+                  .deleteAll()
+                  .typeString('Flutter')
+                  .pauseFor(2000)
+                  .deleteAll()
+                  .typeString('React JS')
+                  .pauseFor(2000)
+                  .deleteAll()
+                  .typeString('Flutter')
+                  .pauseFor(2000)
+                  .deleteAll()
+                  .start()
+              }}
+            />
+          </div>
+
+          <h1 className='col-12 col-md-8 px-0 mx-0 mt-1 fs-1 '>Developer</h1>
+        </div>
+        <div className=' row mt-5'>
+          <h1 className='col-12 col-md-5 px-0 mx-0 fs-4'>
+            Currently working at
+          </h1>
+          <h1 className='col-12 col-md-4 fw-bold mx-0 px-0 fs-4 text-danger'>
+            ProductBox
+          </h1>
+        </div>
+        <div className='row'>
+          <h1 className='col-12 px-0 mx-0 col-md-1 fs-4'>As</h1>
+          <h1 className='col-12 col-md-6 mx-0 px-0 fw-bold fs-4 text-danger'>
+            React JS Intern
+          </h1>
+        </div>
+      </div>
+      <div
+        Style='overflow: hidden'
+        id='image'
+        className='col-6 border bg-danger bg-opacity-75 border rounded-pill'
+      >
+        <img Style='max-width: 100%; height: 90vh' src={img} alt='' />
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/header/Header.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import img from '../../images/danyal.jpg'
+// import '../../index.css'
+
+const Header = (props) => {
+  return (
+    <>
+      <header>
+        <nav class='navbar navbar-expand-lg navbar-light '>
+          <div class='container-fluid'>
+            <Link className='col-4 d-flex nav-link align-content-center' to='/'>
+              <div
+                id='img'
+                class='crop border border-dark border-2 rounded-circle'
+              >
+                <img src={img} alt='Danyal' />
+              </div>
+              <h1 className='mt-2 text-uppercase btn text-dark fw-bold fs-4'>
+                Danyal Ahmad
+              </h1>
+            </Link>
+            <button
+              class='navbar-toggler'
+              type='button'
+              data-bs-toggle='collapse'
+              data-bs-target='#navbarNavDropdown'
+              aria-controls='navbarNavDropdown'
+              aria-expanded='false'
+              aria-label='Toggle navigation'
+            >
+              <span class='navbar-toggler-icon'></span>
+            </button>
+            <div
+              className='collapse navbar-collapse'
+              id='navbarSupportedContent'
+            >
+              <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+                <li className='nav-item '>
+                  <Link
+                    className='nav-link fw-bold text-uppercase mx-3'
+                    aria-current='page'
+                    to='/'
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li className='nav-item'>
+                  <Link
+                    className='nav-link fw-bold text-uppercase mx-3'
+                    to='/BirdsInformation'
+                  >
+                    Resume
+                  </Link>
+                </li>
+                <li className='nav-item'>
+                  <Link
+                    className='nav-link fw-bold text-uppercase mx-3'
+                    to='/Donations'
+                  >
+                    Projects
+                  </Link>
+                </li>
+                <li className='nav-item'>
+                  <Link className='nav-link fw-bold text-uppercase mx-3' to='/'>
+                    Education
+                  </Link>
+                </li>
+                <li className='nav-item'>
+                  <Link
+                    className='nav-link fw-bold text-uppercase mx-3'
+                    to='/BirdSell'
+                  >
+                    About Me
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </>
+  )
+}
+
+export default Header
