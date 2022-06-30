2bba0e9bd6e70043751426cda7774d9ef0ae0898
danyal-deekoders <deekoders@gmail.com>
2022-07-01 00:15:30 +0500

    a

---
+++ build/precache-manifest.f774a97a460927f4d1c24a5458b062dc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d12b8e2aa0feb359fdca",
+    "url": "/static/css/main.b4cc23d8.chunk.css"
+  },
+  {
+    "revision": "d12b8e2aa0feb359fdca",
+    "url": "/static/js/main.d12b8e2a.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "95d4dd04ccfec668800e",
+    "url": "/static/css/2.8f7491fa.chunk.css"
+  },
+  {
+    "revision": "95d4dd04ccfec668800e",
+    "url": "/static/js/2.95d4dd04.chunk.js"
+  },
+  {
+    "revision": "82555cf690c0363f7ef9",
+    "url": "/static/js/runtime~main.82555cf6.js"
+  },
+  {
+    "revision": "b8cd972c9b5567075a4ec77edb154aec",
+    "url": "/static/media/danyal.b8cd972c.jpg"
+  },
+  {
+    "revision": "743edb19758c9582af0e5c276c31a5e6",
+    "url": "/static/media/danyal.743edb19.png"
+  },
+  {
+    "revision": "68d8842b5be0b51465ad91d913b822e7",
+    "url": "/index.html"
+  }
+];
