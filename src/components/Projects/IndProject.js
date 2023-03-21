94e7815ff9d7b417318b55df75d165b865e15751
danyal-deekoders <deekoders@gmail.com>
2023-03-21 13:21:16 +0500

    3rd project

--- src/components/Projects/IndProject.js
+++ src/components/Projects/IndProject.js
@@ -1,15 +1,19 @@
 import React from "react";
-import { Link, Redirect } from "react-router-dom";
 
 function IndProject(props) {
   return (
     <div class="card col-3 my-3 mx-3 text-center">
-      <img src={props.img} className="card-img-top" alt="..." />
+      <img src={props.img} height="200px" className="card-img-top" alt="..." />
       <div className="card-body text-center">
         <h5 className="card-title text-uppercase text-dark">{props.name}</h5>
-        <p className="card-text text-dark">{props.desc}</p>
+        <p
+          className="card-text text-dark"
+          style={{ height: 50, overflow: "scroll" }}
+        >
+          {props.desc}
+        </p>
         <button
-          className="btn btn-dark text-center"
+          className="btn btn-dark text-center align-items-end"
           onClick={() => {
             window.open(props.link);
           }}
