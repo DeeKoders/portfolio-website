b4daef29cc4c06a286dcd9809ac3bbfd5c4c9579
danyal-deekoders <deekoders@gmail.com>
2023-03-21 18:55:18 +0500

    hover effects and headear converted to react code

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,81 +1,67 @@
 import React from "react";
+import { Navbar, Nav, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import img from "../../images/danyal.jpg";
-// import '../../index.css'
-
-const Header = (props) => {
+function Header() {
   return (
-    <>
-      <header className="mb-5">
-        <nav class="navbar navbar-expand-lg navbar-light ">
-          <div class="container-fluid">
-            <Link className="col-4 d-flex nav-link align-content-center" to="/">
-              <div
-                id="img"
-                class="crop border border-dark border-2 rounded-circle"
-              >
-                <img src={img} alt="Danyal" />
-              </div>
-              <h1 className="mt-2 text-uppercase btn text-white fw-bold fs-4">
-                Danyal Ahmad
-              </h1>
-            </Link>
-            <button
-              class="navbar-toggler bg-light "
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNavDropdown"
-              aria-controls="navbarNavDropdown"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="navbar-toggler-icon"></span>
-            </button>
+    <Navbar
+      bg="none"
+      expand="lg"
+      className=" d-flex justify-content-between p-3"
+    >
+      <Col md={4} className="d-flex">
+        <Navbar.Brand href="#">
+          <Link className="d-flex nav-link align-content-center" to="/">
             <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
+              id="img"
+              className="crop border border-dark border-2 rounded-circle"
             >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item ">
-                  <Link
-                    className="nav-link fw-bold text-uppercase mx-3 text-white"
-                    aria-current="page"
-                    to="/"
-                  >
-                    Home
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    className="nav-link fw-bold text-uppercase mx-3 text-white"
-                    to="/skills"
-                  >
-                    Skills
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    className="nav-link fw-bold text-uppercase mx-3 text-white"
-                    to="/projects"
-                  >
-                    Projects
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link
-                    className="nav-link fw-bold text-uppercase mx-3 text-white"
-                    to="/education"
-                  >
-                    Education
-                  </Link>
-                </li>
-              </ul>
+              <img src={img} alt="Danyal" />
             </div>
-          </div>
-        </nav>
-      </header>
-    </>
+            <h1 className="mt-2 text-uppercase btn text-white fw-bold fs-4">
+              Danyal Ahmad
+            </h1>
+          </Link>
+        </Navbar.Brand>
+      </Col>
+      <Col md={8}>
+        <Navbar.Toggle aria-controls="navbarSupportedContent" />
+        <Navbar.Collapse id="navbarSupportedContent">
+          <Nav className="me-auto">
+            <Link
+              className="nav-link fw-bold text-uppercase mx-3 text-white nav-item-custom"
+              aria-current="page"
+              to="/"
+            >
+              Home
+            </Link>
+
+            <Link
+              className="nav-link fw-bold text-uppercase mx-3 text-white nav-item-custom"
+              to="/skills"
+            >
+              Skills
+            </Link>
+            <Link
+              className="nav-link fw-bold text-uppercase mx-3 text-white nav-item-custom"
+              to="/projects"
+            >
+              Projects
+            </Link>
+            <Link
+              className="nav-link fw-bold text-uppercase mx-3 text-white nav-item-custom"
+              to="/education"
+            >
+              Education
+            </Link>
+            <Nav.Link href="#" disabled>
+              Disabled
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Col>
+    </Navbar>
   );
-};
+}
 
 export default Header;
